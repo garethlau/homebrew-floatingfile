c771448c9c76f568b6a534fabf5d35da7c42db7c
Gareth Lau <Gareth.kh.lau@gmail.com>
2022-01-15 22:41:02 -0700

    Update formula

--- Formula/floatingfile.rb
+++ Formula/floatingfile.rb
@@ -3,7 +3,7 @@ class Floatingfile < Formula
     homepage "https://floatingfile.space"
     url "https://github.com/garethlau/homebrew-floatingfile/releases/download/v3.4.0/floatingfile-macos-x64-v3.4.0.tar.gz"
     sha256 "685e3b613d543449c500742d72bcc6c59bed61cc5b63cbf86f17af0640e20630"
-    version "0.1.0"
+    version "3.4.0"
     def install
       bin.install "floatingfile"
     end
